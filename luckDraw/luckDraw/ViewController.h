5928f907cf2ae078d1830253a9bf3cfc98d63a89
BB <yyc95102@vip.qq.com>
2015-09-22 00:27:29 +0800

    initDemo
    This is empty project

---
+++ luckDraw/luckDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  luckDraw
+//
+//  Created by  bb on 15/9/22.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ luckDraw/luckDraw/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  luckDraw
+//
+//  Created by  bb on 15/9/22.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
